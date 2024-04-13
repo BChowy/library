bbe231ef20bd7c700354c55d40eab307c069fce8
Bushra <75831792+BChowy@users.noreply.github.com>
2024-04-13 09:23:54 +0300

    Create objects of books and store it in the list

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Library
+const
+    LIBRARY = document.querySelector('.library'),
+    EMPTY_MESSAGE = document.querySelector('.empty'),
+    ADD_BTN = document.querySelector('.add-btn');
+
+// Dialog
+const
+    DIALOG = document.querySelector("dialog"),
+    TITLE_FIELD = document.querySelector('#book-title'),
+    AUTHOR_FIELD = document.querySelector('#book-author'),
+    PAGES_FIELD = document.querySelector('#book-pages'),
+    READ_CHECKBOX = document.querySelector('#is-read'),
+    CONFIRM_BTN = document.querySelector('.confirm'),
+    CANCEL_BTN = document.querySelector('.cancel');
+
 const BOOK_LIST = [];
 
 function Book(title, author, pages, read) {
@@ -11,4 +27,13 @@ function Book(title, author, pages, read) {
 Book.prototype.addToList = function () {
     BOOK_LIST.push(this);
 };
+
+ADD_BTN.addEventListener("click", () => {
+    DIALOG.showModal();
+});
+
+CONFIRM_BTN.addEventListener("click", () => {
+    const book = new Book(TITLE_FIELD.value, AUTHOR_FIELD.value, PAGES_FIELD.value, READ_CHECKBOX.checked)
+    book.addToList();
+});
 
